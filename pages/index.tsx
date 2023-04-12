228d1250593b0863334a6376519c38ae1470b045
= <cadastro@diegogaspar.dev.br>
2023-04-12 11:22:43 -0300

    Feat Header

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,7 @@
-import { ProductCard } from '@/src/components/ProductCard';
+import { Header } from "@/src/components/header";
 
 export default function Home() {
-  return (
-    <ProductCard
-      img="/image/car.png"
-      title="Product title stays here - max 1 line Product title stays here - max 1 line"
-      desc="Lorem Ipsum is simply dummy text of the printing and typesetting
-    industry. Lorem Lorem Ipsum is simply dummy text of the printing and typesetting
-    industry. Lorem Lorem Ipsum is simply dummy text of the printing and typesetting
-    industry. Lorem"
-      imageProfile="/image/profile.png"
-      nameProfile="Diego Monteiro"
-      km="100 KM"
-      age="2019"
-      price="R$00.000,00"
-    />
-  );
+  return <>
+    <Header/>
+  </>;
 }
---
+++ src/styles/header.ts
@@ -0,0 +1,73 @@
+import styled from 'styled-components';
+
+export const HeaderStyled = styled.header`
+  border-bottom: 2px solid #dee2e6;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  > img {
+    padding: 2.7rem 1.6rem;
+  }
+
+  > nav {
+    background-color: var(--color-grey-10);
+    .computer {
+      display: none;
+    }
+    .computerSpan {
+      display: none;
+    }
+    > div {
+      position: relative;
+      > svg {
+        padding-right: 2.9rem;
+      }
+      > span {
+        width: 100vw;
+        position: absolute;
+        top: 3.5rem;
+        right: 0;
+        background-color: var(--color-whiteFixed);
+        > div {
+          display: flex;
+          flex-direction: column;
+          gap: 4.4rem;
+          padding: 1.2rem;
+        }
+      }
+    }
+    > span {
+    }
+  }
+  @media (min-width: 768px) {
+    > img {
+      padding: 2.7rem 6rem;
+    }
+    > nav {
+      background-color: var(--color-grey-10);
+      border-left: 2px solid var(--color-grey-6);
+      .mobile {
+        display: none;
+      }
+      .computer {
+        display: block;
+        display: flex;
+        flex-direction: row;
+        padding: 1.6rem 6rem;
+        gap: 4.4rem;
+      }
+      .computerSpan {
+        display: flex;
+        flex-direction: column;
+        gap: 1.6rem;
+        padding: 2.2rem;
+        width: 20rem;
+        position: absolute;
+        right: 0;
+        background-color: var(--color-grey-9);
+        border-radius: 0.4rem;
+      }
+    }
+  }
+`;
