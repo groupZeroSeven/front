533e5c0fed255bd6180f7893269bae7b0ed9e73c
Gabriel Ogawa <gabriel.ogawa@hotmail.com>
2023-04-25 18:50:53 -0300

    feat: add color, year and fuel filter

--- src/components/filter.tsx
+++ src/components/filter.tsx
@@ -17,6 +17,9 @@ export const Filter = () => {
 
   const [listBrands, setListBrands] = useState<string[]>(brands)
   const [listModels, setListModels] = useState<string[]>(models)
+  const [listColors, setListColors] = useState<string[]>(colors)
+  const [listYears, setListYears] = useState<string[]>(years)
+  const [listFuels, setListFuels] = useState<string[]>(fuels)
 
   const [markedBrand, setMarkedBrand] = useState<string>("")
   const [markedModel, setMarkedModel] = useState<string>("")
@@ -43,18 +46,21 @@ export const Filter = () => {
     event: React.MouseEvent<HTMLButtonElement>
   ): void => {
     setMarkedColor(event.currentTarget.value);
+    setListColors([event.currentTarget.value])
   };
 
   const handleYearMarked = (
     event: React.MouseEvent<HTMLButtonElement>
   ): void => {
     setMarkedYear(event.currentTarget.value);
+    setListYears([event.currentTarget.value])
   };
 
   const handleFuelMarked = (
     event: React.MouseEvent<HTMLButtonElement>
   ): void => {
     setMarkedFuel(event.currentTarget.value);
+    setListFuels([event.currentTarget.value])
   };
 
   const resetFilter = () => {
@@ -66,29 +72,51 @@ export const Filter = () => {
     setMarkedYear("")
     setListBrands(brands)
     setListModels(models)
+    setListColors(colors)
+    setListYears(years)
+    setListFuels(fuels)
   }
 
-  const modelLister = (list: iModel[], filterAdverts: iAdvert[]) => {
+  const filterLister = (filterAdverts: iAdvert[]) => {
+    const colorList = filterAdverts.map((car) => (car.color.split(" ")[0].toLocaleLowerCase()))
+    const yearList = filterAdverts.map((car) => (car.year))
+    const fuelList = filterAdverts.map((car) => (car.fuel))
+
+    const uniqueColorArr = colorList.filter((elem: string, index: number, self: string[]) => {
+      return index === self.indexOf(elem);
+    });
+    const uniqueYearArr = yearList.filter((elem: string, index: number, self: string[]) => {
+      return index === self.indexOf(elem);
+    });
+    const uniqueFuelArr = fuelList.filter((elem: string, index: number, self: string[]) => {
+      return index === self.indexOf(elem);
+    });
+
+    const upperColor = uniqueColorArr.map((colorName) => (`${colorName.split(" ")[0][0].toUpperCase()}${colorName.split(" ")[0].slice(1).toLowerCase()}`))
+    
+    setListColors(upperColor);
+    setListYears(uniqueYearArr);
+    setListFuels(uniqueFuelArr);
+  }
+  const filterModel = (list: iModel[], filterAdverts: iAdvert[]) => {
     const modelsList = list.map((model: iModel) => (model.name.split(" ")[0]))
-    const uniqueArr = modelsList.filter((elem: string, index: number, self: string[]) => {
+
+    const uniqueModelArr = modelsList.filter((elem: string, index: number, self: string[]) => {
       return index === self.indexOf(elem);
     });
 
     const coincidenceList = []
     for (let i = 0; i < filterAdverts.length; i++) {
-      if (uniqueArr.includes(filterAdverts[i].model.split(" ")[0])) {
+      if (uniqueModelArr.includes(filterAdverts[i].model.split(" ")[0])) {
         coincidenceList.push(filterAdverts[i].model)
       }
     }
-
     const upperName = coincidenceList.map((modelName) => (`${modelName.split(" ")[0][0].toUpperCase()}${modelName.split(" ")[0].slice(1).toLowerCase()}`))
     const uniqueModels = upperName.filter((elem: string, index: number, self: string[]) => {
       return index === self.indexOf(elem);
     })
-
     setListModels(uniqueModels);
   }
-
   useEffect(() => {
     if (filteredAdverts) {
       const filter = filteredAdverts.filter((advert) => (advert.brand === markedBrand))
@@ -109,7 +137,8 @@ export const Filter = () => {
         );
         if (adverts) {
           const filterAdverts = adverts.filter((advert) => (advert.brand === markedBrand))
-          modelLister(data, filterAdverts)
+          filterModel(data, filterAdverts)
+          filterLister(filterAdverts)
         }
       } catch (err) {
         console.log(err);
@@ -129,7 +158,7 @@ export const Filter = () => {
       })
       
       setFilteredAdverts(filter)
-      
+      filterLister(filteredAdverts)
   } 
   if (!filteredAdverts && adverts) {
     const filter = adverts.filter((advert) => {
@@ -139,7 +168,7 @@ export const Filter = () => {
     })
     
     setFilteredAdverts(filter)
-    
+    filterLister(adverts)
   }
   
   }, [markedModel])
@@ -147,15 +176,15 @@ export const Filter = () => {
   useEffect(() => {
     if (filteredAdverts) {
       const filter = filteredAdverts.filter((advert) => (advert.color.toLocaleLowerCase() === markedColor.toLocaleLowerCase()))
-      if (filter.length > 0) {
-        setFilteredAdverts(filter)
-      }
+      
+      setFilteredAdverts(filter)
+      filterLister(filteredAdverts)
     } 
     if (!filteredAdverts && adverts) {
       const filter = adverts.filter((advert) => (advert.color.toLocaleLowerCase() === markedColor.toLocaleLowerCase()))
-      if (filter.length > 0) {
+      
         setFilteredAdverts(filter)
-      }
+        filterLister(adverts)
   }
   
   }, [markedColor])
@@ -163,30 +192,32 @@ export const Filter = () => {
   useEffect(() => {
     if (filteredAdverts) {
       const filter = filteredAdverts.filter((advert) => (advert.year === markedYear))
-      if (filter.length > 0) {
-        setFilteredAdverts(filter)
-      }
+      
+      setFilteredAdverts(filter)
+      
+      filterLister(filteredAdverts)
     } 
     if (!filteredAdverts && adverts) {
       const filter = adverts.filter((advert) => (advert.year === markedYear))
-      if (filter.length > 0) {
-        setFilteredAdverts(filter)
-      }
+     
+      setFilteredAdverts(filter)
+      filterLister(adverts)
     }
   }, [markedYear])
 
   useEffect(() => {
     if (filteredAdverts) {
       const filter = filteredAdverts.filter((advert) => (advert.fuel === markedFuel))
-      if (filter.length > 0) {
-        setFilteredAdverts(filter)
-      }
+      
+      setFilteredAdverts(filter)
+      
+      filterLister(filteredAdverts)
     } 
     if (!filteredAdverts && adverts) {
       const filter = adverts.filter((advert) => (advert.fuel === markedFuel))
-      if (filter.length > 0) {
-        setFilteredAdverts(filter)
-      }
+      
+      setFilteredAdverts(filter)
+      filterLister(adverts)
     }
   }, [markedFuel])
     return(
@@ -196,7 +227,7 @@ export const Filter = () => {
                 <Heading_4_600>Marca</Heading_4_600>
                     {
                       listBrands.map((brand) => (
-                        <li key={brand}><button value={brand} onClick={handleBrandMarked} style={markedBrand === brand ? {backgroundColor: "red"} : {backgroundColor: "blue"}}><Heading_6_500>{brand}</Heading_6_500></button></li>
+                        <li key={brand}><button value={brand} onClick={handleBrandMarked}><Heading_6_500>{brand}</Heading_6_500></button></li>
                       ))
                     }
                 </ul>
@@ -204,15 +235,15 @@ export const Filter = () => {
                     <Heading_4_600>Modelo</Heading_4_600>
                     {
                       listModels.map((model) => (
-                        <li key={model}><button value={model} onClick={handleModelMarked} style={markedModel === model ? {backgroundColor: "red"} : {backgroundColor: "blue"}}><Heading_6_500>{model}</Heading_6_500></button></li>
+                        <li key={model}><button value={model} onClick={handleModelMarked} ><Heading_6_500>{model}</Heading_6_500></button></li>
                       ))
                     }
                 </ul>
                 <ul>
                     <Heading_4_600>Cor</Heading_4_600>
                     {
-                      colors.map((color) => (
-                        <li key={color}><button value={color} onClick={handleColorMarked} style={markedColor === color ? {backgroundColor: "red"} : {backgroundColor: "blue"}}><Heading_6_500>{color}</Heading_6_500></button></li>
+                      listColors.map((color) => (
+                        <li key={color}><button value={color} onClick={handleColorMarked} ><Heading_6_500>{color}</Heading_6_500></button></li>
                       ))
                     }
                    
@@ -221,16 +252,16 @@ export const Filter = () => {
                 <ul>
                     <Heading_4_600>Ano</Heading_4_600>
                     {
-                      years.map((year) => (
-                        <li key={year}><button value={year} onClick={handleYearMarked} style={markedYear === year ? {backgroundColor: "red"} : {backgroundColor: "blue"}}><Heading_6_500>{year}</Heading_6_500></button></li>
+                      listYears.map((year) => (
+                        <li key={year}><button value={year} onClick={handleYearMarked} ><Heading_6_500>{year}</Heading_6_500></button></li>
                       ))
                     }
                 </ul>
 
                 <ul>
                     <Heading_4_600>Combustivel</Heading_4_600>
-                    {fuels.map((fuel) => (
-                      <li key={fuel}><button value={fuel} onClick={handleFuelMarked} style={markedFuel === fuel ? {backgroundColor: "red"} : {backgroundColor: "blue"}}><Heading_6_500>{fuel}</Heading_6_500></button></li>
+                    {listFuels.map((fuel) => (
+                      <li key={fuel}><button value={fuel} onClick={handleFuelMarked} ><Heading_6_500>{fuel}</Heading_6_500></button></li>
                     ))}
                 </ul>
                 <form>
