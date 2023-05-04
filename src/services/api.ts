1bdfa0ca49e2b13255600dec48bf5163bc345ebe
= <cadastro@diegogaspar.dev.br>
2023-05-04 12:26:22 -0300

    Fix url

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,11 +1,12 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'http://localhost:8000',
+  // baseURL: 'http://localhost:8000',
+  baseURL: 'https://project-sevenzero.onrender.com',
   timeout: 5000,
 });
 
 export const apiKars = axios.create({
   baseURL: 'https://kenzie-kars.herokuapp.com',
   timeout: 5000,
-})
+});
